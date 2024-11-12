b6e078787b89d45c3014bcab6bcd038fc3c7b411
zhaoyu93 <zhaoyu93@meituan.com>
2024-11-12 22:12:45 +0800

    2024-11-12 服务注册成功

--- renren-admin/src/main/java/io/renren/AdminApplication.java
+++ renren-admin/src/main/java/io/renren/AdminApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author Mark sunlightcs@gmail.com
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
